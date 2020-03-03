66153de990707d3ac23138cd07da74bb3ab7395b
omgkali <looksagar59@gmail.com>
2020-03-03 10:50:55 +0600

    first commit

---
+++ app/src/main/java/com/b1/ltd/sharedpreference/MainActivity.java
@@ -0,0 +1,25 @@
+package com.b1.ltd.sharedpreference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/b1/ltd/sharedpreference/MyService.java
@@ -0,0 +1,21 @@
+package com.b1.ltd.sharedpreference;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import androidx.annotation.Nullable;
+
+public class MyService extends IntentService {
+
+
+    public MyService() {
+        super("MyService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+
+
+    }
+}
